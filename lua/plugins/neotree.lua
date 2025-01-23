a0f9ec23fe01c4f39e34acd308c6d8e357f626d3
ThatAdwaithGuy <106865321+ThatAdwaithGuy@users.noreply.github.com>
2025-01-23 11:30:28 +0530

    changed neotree keybind

--- lua/plugins/neotree.lua
+++ lua/plugins/neotree.lua
@@ -2,7 +2,7 @@ return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
   keys = {
-    {"<C-n>", ":Neotree filesystem reveal right<CR>", desc = "Open file viewer"}
+    {"<C-n>", ":Neotree toggle filesystem reveal right<CR>", desc = "Open file viewer"}
   },
     dependencies = {
       "nvim-lua/plenary.nvim",
