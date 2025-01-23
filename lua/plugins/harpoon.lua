91b5646c4fe4e22377e310267bcd9d27cb8816f2
ThatAdwaithGuy <106865321+ThatAdwaithGuy@users.noreply.github.com>
2025-01-23 11:43:20 +0530

    changed keybind

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -11,9 +11,9 @@ return {
     vim.keymap.set("n", "<C-e>", function() harpoon.ui:toggle_quick_menu(harpoon:list()) end)
     
     vim.keymap.set("n", "<C-h>", function() harpoon:list():select(1) end)
-    vim.keymap.set("n", "<C-s>", function() harpoon:list():select(2) end)
+    vim.keymap.set("n", "<C-w>", function() harpoon:list():select(2) end)
     vim.keymap.set("n", "<C-S-h>", function() harpoon:list():select(3) end)
-    vim.keymap.set("n", "<C-S-s>", function() harpoon:list():select(4) end)
+    vim.keymap.set("n", "<C-S-w>", function() harpoon:list():select(4) end)
   end,
 
 }
